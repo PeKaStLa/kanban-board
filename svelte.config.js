5a0766cc652d86959f8961b75f7a93e67e0e0e86
PeKaStLa <14peterstadler@gmail.com>
2021-06-29 00:03:06 +0200

    add node adapter for building

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,4 +13,11 @@ const config = {
 	}
 };
 
-export default config;
+
+export default {
+	kit: {
+		adapter: node({ out: 'kanban-output-directory' })
+	}
+};
+
+//export default config;

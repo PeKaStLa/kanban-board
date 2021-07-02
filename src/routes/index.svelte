b90597ca2e0218aaa78c9af17f733a6f6f42034d
PeKaStLa <14peterstadler@gmail.com>
2021-07-02 18:19:33 +0200

    title overflow

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,10 +22,8 @@
 		const _RES_JSON = await _RES.json();
 		if (_RES.status == 200) {
 			_ARR.push(_RES_JSON[0].text);
-			_TODO = _TODO;
-			_PROGRESS = _PROGRESS;
-			_DONE = _DONE;
 			console.log(_URL_PARAM + ' - status: 200');
+			return _ARR;
 		}
 		if (_RES.status == 400) {
 			console.log(_URL_PARAM + ' - status: 400');
@@ -34,9 +32,9 @@
 	}
 
 	onMount(async () => {
-		_get_init_items(_TODO, 'http://peterstadler.com:7000/', 'todo');
-		_get_init_items(_PROGRESS, 'http://peterstadler.com:7000/', 'progress');
-		_get_init_items(_DONE, 'http://localhost:8000/', 'done');
+		_TODO = await _get_init_items(_TODO, 'http://peterstadler.com:7000/', 'todo');
+		_PROGRESS = await _get_init_items(_PROGRESS, 'http://peterstadler.com:7000/', 'progress');
+		_DONE = await _get_init_items(_DONE, 'http://localhost:8000/', 'done');
 	});
 
 	beforeUpdate(() => {
@@ -108,7 +106,7 @@
 		rounded-t-md border-gray-500  mr-2 
 		{is_empty(_TODO) ? 'rounded-b-md  border-b-2' : ''}"
 		>
-			<h2 class="text-center text-2xl">ToDo:</h2>
+		<h2 class=" overflow-ellipsis text-center overflow-hidden  text-2xl">ToDo:</h2>
 			<div
 				class="text-center  overflow-hidden rounded-md 
 border-2  bg-gray-500  
@@ -116,14 +114,14 @@ border-gray-500 p-1 m-2"
 			>
 				<div class="inline-block   rounded-sm p-2">
 					<input
-						class="rounded-sm"
+						class="rounded-sm w-full"
 						on:keypress={_on_key_press_todo}
 						bind:value={_TEXTFIELD_TODO}
 						placeholder="enter an Item"
 					/>
 				</div>
 				<div class="inline-block">
-					<button class="bg-gray-100 rounded-md p-2" on:click={() => _add_todo(_TEXTFIELD_TODO)}
+					<button class="bg-gray-100 rounded-md p-2 " on:click={() => _add_todo(_TEXTFIELD_TODO)}
 						>Add Item</button
 					>
 				</div>
@@ -134,15 +132,15 @@ border-gray-500 p-1 m-2"
 			 rounded-t-md border-gray-500 mr-2 
 			 {is_empty(_PROGRESS) ? 'rounded-b-md  border-b-2' : ''}"
 		>
-			<h2 class=" text-center text-2xl">In Progress:</h2>
+			<h2 class=" overflow-ellipsis text-center overflow-hidden  text-2xl">In Progress:</h2>
 			<div
-				class="text-center  overflow-hidden rounded-md 
+				class="text-center  rounded-md 
 border-2  bg-gray-500  
 border-gray-500 p-1 m-2"
 			>
 				<div class="inline-block   rounded-sm p-2">
 					<input
-						class="rounded-sm"
+						class="rounded-sm w-full"
 						on:keypress={_on_key_press_progress}
 						bind:value={_TEXTFIELD_PROGRESS}
 						placeholder="enter an Item"
@@ -160,7 +158,7 @@ border-gray-500 p-1 m-2"
 			class="col-span-1 border-t-2 bg-gray-300  border-l-2 border-r-2 
 			rounded-t-md border-gray-500  {is_empty(_DONE) ? 'rounded-b-md  border-b-2' : ''}"
 		>
-			<h2 class="text-center  text-2xl">Done:</h2>
+		<h2 class=" overflow-ellipsis text-center overflow-hidden  text-2xl">Done:</h2>
 			<div
 				class="text-center  overflow-hidden rounded-md 
 border-2  bg-gray-500  
@@ -168,7 +166,7 @@ border-gray-500 p-1 m-2"
 			>
 				<div class="inline-block   rounded-sm p-2">
 					<input
-						class="rounded-sm"
+						class="rounded-sm w-full"
 						on:keypress={_on_key_press_done}
 						bind:value={_TEXTFIELD_DONE}
 						placeholder="enter an Item"

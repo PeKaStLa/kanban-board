b21c00170c701a56ddb5087000c6d584e555223f
PeKaStLa <14peterstadler@gmail.com>
2021-07-03 02:00:07 +0200

    words too long for row are broken in parts now

--- src/routes/item.svelte
+++ src/routes/item.svelte
@@ -2,4 +2,4 @@
 	export let _ITEM = 'default empty';
 </script>
 <p class="overflow-ellipsis text-center overflow-hidden  
- border-2 rounded-md  my-2 p-2 bg-gray-100">{_ITEM}</p>
+ border-2 rounded-md  my-2 p-2 break-words bg-gray-100">{_ITEM}</p>

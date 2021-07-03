247faf76efce9609645d3e0b29560e705ac2e2c7
PeKaStLa <14peterstadler@gmail.com>
2021-07-03 19:15:47 +0200

    small fix

--- src/routes/item.svelte
+++ src/routes/item.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
 	export let _ITEM = 'default empty';
 </script>
-<p class="overflow-ellipsis text-center overflow-hidden  
- border-2 rounded-md  my-2 p-2 break-words bg-gray-100">{_ITEM}</p>
+<p class="overflow-ellipsis text-center overflow-hidden  border-gray-300
+ border-2 rounded-md  m-2 p-2 break-words bg-gray-100">{_ITEM}</p>

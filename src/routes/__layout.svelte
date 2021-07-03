64960b769fcb3b9cac9992bd1bf6c171005b07e7
PeKaStLa <14peterstadler@gmail.com>
2021-07-03 20:08:49 +0200

    moved pb from layout to pt in index for h-screen

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,8 @@
 <!-- src/routes/__layout.svelte -->
 <script lang="ts">
 </script>
-
-<div class=" pb-14 ">
+<!-- pb-14  -->
+<div class=" ">
 	<nav class="bg-gray-400 fixed w-full justify-center text-center z-50  ">
 		<h1 class="text-3xl font-normal w-auto text-black p-2 ">Kanban-Board</h1>
 	</nav>

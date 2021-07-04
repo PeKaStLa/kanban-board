17413cd27d3991628cf21d296507029ad069d187
PeKaStLa <14peterstadler@gmail.com>
2021-07-04 03:46:12 +0200

    perfekt options-button for every item. transparent  until hover

--- src/routes/item.svelte
+++ src/routes/item.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
 	export let _ITEM = 'default empty';
 </script>
-<p class="overflow-ellipsis text-center overflow-hidden  border-gray-300
- border-2 rounded-md  m-2 p-2 break-words bg-gray-100">{_ITEM}</p>
+
+<div
+	class="relative overflow-ellipsis text-center overflow-hidden  
+border-gray-300
+ border-2 rounded-md  m-1.5 p-1.5 break-words bg-gray-100  "
+>
+	{_ITEM}
+	<div
+		class="absolute top-1 right-1    text-center  w-min bg-blue-300 rounded-md hover:text-white
+	text-transparent hover:bg-gray-500 bg-transparent  pr-1.5 pt-0.5 pl-0.5  "
+	>
+		<button>
+			<svg class="   h-5 w-5 " xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+			</svg>
+		</button>
+	</div>
+
+</div>
